e1b4c0ed5f3434194e87aa3b9aee2b510b71e90e
Joao Evangelista <evangelistajoaop@gmail.com>
2015-07-19 00:59:06 -0300

    fixed glicth with glich

--- src/main/java/io/curly/commons/github/EnableGitHubAuthenticationResolver.java
+++ src/main/java/io/curly/commons/github/EnableGitHubAuthenticationResolver.java
@@ -1,7 +1,16 @@
 package io.curly.commons.github;
 
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
 /**
  * @author João Pedro Evangelista
  */
+@Documented
+@Inherited
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Import({GitHubResolverAutoConfiguration.class})
 public @interface EnableGitHubAuthenticationResolver {
 }
